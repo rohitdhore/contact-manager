76f7ebe0597e4b1da00bb78ba87a37af356ff270
Rohit Dhore <rohit.dhore@appdirect.com>
2019-08-16 14:39:00 +0530

    contact form unit test added

--- src/ContactForm.test.js
+++ src/ContactForm.test.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ContactForm from './ContactForm';
+
+const contactForm = shallow(<ContactForm/>);
+
+it('should render correctly', () => {
+	expect(contactForm).toMatchSnapshot();
+});
