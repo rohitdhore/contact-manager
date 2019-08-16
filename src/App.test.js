8bc1c621efba83ee8f339499021b2d94be194ac7
Rohit Dhore <rohit.dhore@appdirect.com>
2019-08-16 09:12:27 +0530

    Initial commit from Create React App

--- src/App.test.js
+++ src/App.test.js
@@ -7,9 +7,3 @@ const app = shallow(<App />);
 it('renders correctly', () => {
   expect(app).toMatchSnapshot();
 });
-
-//it('renders without crashing', () => {
-//  const div = document.createElement('div');
-//  ReactDOM.render(<App />, div);
-//  ReactDOM.unmountComponentAtNode(div);
-//});
