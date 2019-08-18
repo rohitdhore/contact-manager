e82e202f7a33be350166e18b07be84de639887de
Rohit Dhore <rohit.dhore@appdirect.com>
2019-08-18 20:55:05 +0530

    Updates

---
+++ precache-manifest.bf7d27b8ec57f1f0a73082dae4a27784.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a70523c5d0bad006e0a13c4a8f173f3",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "f22b4731941bcc81882c",
+    "url": "/contact-manager/static/css/main.4ce7c449.chunk.css"
+  },
+  {
+    "revision": "c11c14700878b13c6ce0",
+    "url": "/contact-manager/static/js/2.5afb3949.chunk.js"
+  },
+  {
+    "revision": "f22b4731941bcc81882c",
+    "url": "/contact-manager/static/js/main.b67d61a8.chunk.js"
+  },
+  {
+    "revision": "d9edddcd867f9ff3298c",
+    "url": "/contact-manager/static/js/runtime~main.0ae5329e.js"
+  }
+]);
